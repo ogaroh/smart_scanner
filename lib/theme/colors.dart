19539219cf82de865649f49dcb740e69a0e3661e
ogaroh <ogaroerick1998@gmail.com>
2021-06-29 09:36:31 +0300

    theme

---
+++ lib/theme/colors.dart
@@ -0,0 +1,34 @@
+///
+/// All the colors to be used in the application should be declared & managed here.
+///
+
+import 'package:flutter/material.dart';
+
+Map<int, Color> colorCodes = {
+  50: const Color.fromRGBO(122, 1, 131, .1),
+  100: const Color.fromRGBO(122, 1, 131, .2),
+  200: const Color.fromRGBO(122, 1, 131, .3),
+  300: const Color.fromRGBO(122, 1, 131, .4),
+  400: const Color.fromRGBO(122, 1, 131, .5),
+  500: const Color.fromRGBO(122, 1, 131, .6),
+  600: const Color.fromRGBO(122, 1, 131, .7),
+  700: const Color.fromRGBO(122, 1, 131, .8),
+  800: const Color.fromRGBO(122, 1, 131, .9),
+  900: const Color.fromRGBO(122, 1, 131, 1),
+};
+
+final MaterialColor kPrimarySwatch = MaterialColor(0xFF7A0183, colorCodes);
+const kBrandColor = Color(0xFF7A0183);
+const kDefaultYellow = Color(0xFFfee100);
+const kDefaultRed = Color(0xFFdc3732);
+const kDefaultBlack = Color(0xFF000000);
+const kDarkestGrey = Color(0xFF2e2e2e);
+const kDarkGrey = Color(0xFF666565);
+const kDefaultGrey = Color(0xFFA8AAAB);
+const kLightGrey = Color(0xFFDFE0E2);
+const kDefaultWhite = Color(0xFFffffff);
+const kDarkBlue = Color(0xFF014361);
+const kDefaultBlue = Color(0xFF048bc9);
+const kSuccessGreen = Color(0xFF0ac93d);
+const kWarningOrange = Color(0xFFf29f05);
+const kErrorRed = Color(0xFFa80316);
---
+++ lib/theme/theme.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:smart_scanner/theme/colors.dart';
+
+final ThemeData themeData = ThemeData(
+  primarySwatch: kPrimarySwatch,
+);
