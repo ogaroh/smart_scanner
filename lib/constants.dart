5364bc160206971a2bf7f2996b68d33fe0abea19
ogaroh <ogaroerick1998@gmail.com>
2021-06-29 09:36:26 +0300

    global variables

---
+++ lib/constants.dart
@@ -0,0 +1,9 @@
+///
+/// Author: Erick Ogaro
+/// Copyright, 2021 Alopex Consult
+///
+/// This app contains all the global constants that are required in the app
+///
+
+// Application Name
+const kApplicationName = "Smart Scanner";
